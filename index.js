a6a4bb0093635f63433247395af4609760f34e2a
hina.maeyama <hina.maeyama@add-more.co.jp>
2020-01-02 01:38:53 +0900

    [modify]2file for input form

--- index.js
+++ index.js
@@ -2,12 +2,5 @@ var app= new Vue({
     el: '#app',
     data: {
         message: 'Hello,World!!!!'
-    },
-    
-    methods: {
-        reverseMessage: function(){
-            this.message= this.message.split('').reverse().join('')
-        }
     }
-
 })
