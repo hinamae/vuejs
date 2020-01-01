d2cb01b30c4d430a09a8d6a2987542e777feb922
hina.maeyama <hina.maeyama@add-more.co.jp>
2020-01-02 02:14:30 +0900

    [modify]2file for using vue components more useful

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 Vue.component('todo-item',{
-    template: '<li>this is a todo</li>'
+    props: ['todo'],
+    template: '<li>{{ todo.text }}</li>'
 })
 
 var app= new Vue({
     el: '#app',
+    data: {
+        groceryList: [
+            { id: 0, text: 'vegetables'},
+            { id: 1, text: 'cheese'},
+            { id: 2, text: 'whatever else humans are supposed to eat'}
+        ]
+    }
 })
